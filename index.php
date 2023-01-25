dbec24c15db61638993994f3464cb37d47214722
uakhponomarev <47500608+uakhponomarev@users.noreply.github.com>
2023-01-25 15:48:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,18 @@
 <?php
-//including the database connection file
-include_once("config.php");
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:azuredevops20230122servername.database.windows.net,1433; Database = azuredevops20230122dbname", "dbadmin", getenv('DB_PASSWORD'));
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
-//fetching data in descending order (lastest entry first)
-//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
-$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
-?>
-
-<html>
-<head>	
-	<title>Homepage</title>
-</head>
-
-<body>
-<a href="add.html">Add New Data</a><br/><br/>
-<?php
-echo 'Syntax with DOLLAR. My username is ' .$_ENV["PATH"] . '!';
-	// Example use of getenv()
-	echo 'Syntax with getenv getenv('PATH').'
-	
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "dbadmin", "pwd" => getenv('DB_PASSWORD'), "Database" => "azuredevops20230122dbname", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:azuredevops20230122servername.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
 	<table width='80%' border=0>
 
@@ -28,16 +22,6 @@ echo 'Syntax with DOLLAR. My username is ' .$_ENV["PATH"] . '!';
 		<td>Email</td>
 		<td>Update</td>
 	</tr>
-	<?php 
-	//while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-	while($res = mysqli_fetch_array($result)) { 		
-		echo "<tr>";
-		echo "<td>".$res['name']."</td>";
-		echo "<td>".$res['age']."</td>";
-		echo "<td>".$res['email']."</td>";	
-		echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";		
-	}
-	?>
 	</table>
 </body>
 </html>
